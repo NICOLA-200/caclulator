c0e0e10645d4f513dd482bea0d5579dbbc37ced3
NICOLA-200 <fatepepe66@gmail.com>
2023-04-30 10:03:34 +0200

    input added in the calculator

---
+++ script.js
@@ -0,0 +1,34 @@
+let equal_pressed = 0;
+
+let button_input = document.querySelectorAll(".input-button")
+
+let input = document.getElementById("input")
+
+
+let equal = document.getElementById("equal")
+let clear = document.getElementById("clear")
+
+let erase = document.getElementById("erase")
+
+
+window.onload = () => {
+
+     input.value = "";
+}
+
+button_input.forEach((button_class) => {
+     button_class.addEventListener("click", () => {
+          if(equal_pressed == 1) {
+               input.value = ""
+               equal_pressed = 0;
+          }
+
+          input.value +=button_class.value
+     })
+})
+
+equal.addEventListener("click", () =>{
+     equal_pressed = 1;
+     console.log(equal_pressed)
+     let inp_val = input.value
+})
